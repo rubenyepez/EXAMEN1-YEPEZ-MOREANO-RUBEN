0517e2ee0182a1cc8032ff0543b4089c06a6b252
rubenyepez <60449739+rubenyepez@users.noreply.github.com>
2020-02-03 14:19:56 -0500

    Add files via upload

--- EXAMEN1-YEPEZ-MOREANO-RUBEN/Controllers/PedidosController.vb
+++ EXAMEN1-YEPEZ-MOREANO-RUBEN/Controllers/PedidosController.vb
@@ -3,45 +3,55 @@
 
     '
     ' GET: /Pedidos
-
+    Function Login() As ActionResult
+        ViewData("Sistema") = "PEDIDOS PENDIENTES"
+        ViewData("Modulo") = "INGRESO SISTEMA"
+        Return View()
+    End Function
+    Function VerificaConexion(ByVal Servidor As String, ByVal User As String, ByVal Pass As String) As Integer
+        Dim resultado As Integer
+        Dim miBas As Conexion = New Conexion
+        resultado = miBas.VerificaConexion(Servidor, User, Pass)
+        Session("Servidor") = Servidor
+        Session("User") = User
+        Session("Pass") = Pass
+        Return resultado
+    End Function
     Function listaPedidoPendiente() As ActionResult
-        Dim lista As New List(Of PedidosPendientes)
+        'Dim lista As New List(Of PedidosPendientes)
         Dim usrData As New UtilesPedidos
-        Dim Anno As Integer = CStr(usrData.MaximoAnno)
-        lista = usrData.ListaPedidos(Anno)
+        Dim Anno As Integer = CStr(usrData.MaximoAnno(CStr(Session("Servidor")), CStr(Session("User")), CStr(Session("Pass"))))
+        'lista = usrData.ListaPedidos(Anno)
         ViewData("Encabezado") = "PEDIDOS PENDIENTES"
         ViewData("nombreUsuario") = "Ruben Yepez Moreano"
         ViewData("AnnoActual") = Anno
-        Return View(lista)
+
+        Return View()
     End Function
 
 
-    Function listaCambiaAnnoPedido(ByVal Anno As Integer) As JsonResult
-        Dim lista As New List(Of PedidosPendientes)
+    Function listaCambiaAnnoPedido(ByVal Anno As Integer) As String
+        Dim miStr As String
         Dim usrData As New UtilesPedidos
-        lista = usrData.ListaPedidos(Anno)
-      
-        Return Json(lista, JsonRequestBehavior.AllowGet)
+        miStr = usrData.ListaPedidosString(Anno, CStr(Session("Servidor")), CStr(Session("User")), CStr(Session("Pass")))
+        Return miStr
     End Function
-    Function DetallePedido(ByVal Orden As String) As JsonResult
-        Dim lista As New List(Of modDetallePedido)
+    Function DetallePedido(ByVal Orden As String) As String
+        Dim miString As String
         Dim usrData As New UtilesPedidos
-        Dim total As Double = 0
-        lista = usrData.DetallePedido(Orden, total)
-        Dim strTotal As String = usrData.FormateaNumero(total, 12)
-        ViewData("Total") = strTotal.Trim()
-        lista(0).TotalGeneral = strTotal.Trim()
-        Return Json(lista, JsonRequestBehavior.AllowGet)
+        miString = usrData.DetallePedido(Orden, CStr(Session("Servidor")), CStr(Session("User")), CStr(Session("Pass")))
+
+        Return miString
     End Function
 
-    Function AnnosConsulta() As JsonResult
+    Function AnnosConsulta() As String
         '    Valor As String
         'Public Property Anno As String
         Dim listaAnno = New List(Of modAnnoConsulta)
         Dim usrDocu As New UtilesPedidos
-        listaAnno = usrDocu.AnnosEnConsulta
+        Dim miString As String = usrDocu.AnnosEnConsulta(CStr(Session("Servidor")), CStr(Session("User")), CStr(Session("Pass")))
 
-        Return Json(listaAnno, JsonRequestBehavior.AllowGet)
+        Return (miString)
     End Function
 
     'var miUrl = '/Pedidos/grabaPedido/?Orden=' + ord+'&Fecha='+miFecha+'&Comentario='+miComentario+'&Graba='+graba;
@@ -51,7 +61,7 @@
         Dim i As Integer = 0
 
         Dim usrDocu As New UtilesPedidos
-        i = usrDocu.grabaPedido(Orden, Fecha, Comentario, Graba)
+        i = usrDocu.grabaPedido(Orden, Fecha, Comentario, Graba, CStr(Session("Servidor")), CStr(Session("User")), CStr(Session("Pass")))
 
         Return i
     End Function
